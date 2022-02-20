7501a80eb33ba81dd0d5a4a8f8280390b70daf0c
Silas <silas@sisyfox.com>
2022-02-20 13:00:34 +0100

    get wind and rain data

--- main.py
+++ main.py
@@ -8,8 +8,25 @@ soup = BeautifulSoup(r.content, 'html.parser')
 #s = soup.findAll('div', class_='weathertable__body')
 s = soup.select("div.weathertable__body > div:nth-of-type(8)")
 
-for weather in s.findAll('span'):
-    print(weather)
+print("Min and Max Wind")
+for wind in soup.select("span.units-ws"):
+    print(wind.text) #prints min and max wind variables
+
+print("Rain")
+for rain in soup.select("div.data-rain"):
+    #print(rain.select('span'))
+
+    if(rain.find("span")) != None:
+        print(rain.select('span')[0].text)
+    else:
+        print(0)
+
+#for div in s:
+#    test = div.findAll('span')
+#    print(test)
+#    finalStr+=str(test)
+    
+#print(finalStr[finalStr.find('>')+1 : finalStr.find('</')])
 
 with open('scapes.txt', 'w') as f:
     f.write(str(s))
