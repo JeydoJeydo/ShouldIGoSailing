63d6f93b62e60e1c44b7263257088c3d66a5afd9
Silas <silas@sisyfox.com>
2022-02-20 11:43:07 +0100

    get specific div

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.windfinder.com/forecast/salzgittersee')
+r = requests.get('https://www.windfinder.com/weatherforecast/salzgittersee')
 print(r)
 pulled = r.content.decode("utf-8")
 soup = BeautifulSoup(r.content, 'html.parser')
-s = soup.find('div', class_='fc-day-index-1')
+#s = soup.findAll('div', class_='weathertable__body')
+s = soup.select("div.weathertable__body > div:nth-of-type(8)")
+
+for weather in s.findAll('span'):
+    print(weather)
 
 with open('scapes.txt', 'w') as f:
     f.write(str(s))
