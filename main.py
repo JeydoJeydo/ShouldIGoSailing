7babb76bf9ed5df63e1abdded9a00be81ebed222
Silas <silas@sisyfox.com>
2022-02-20 15:35:38 +0100

    deleted unused code

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
-r = requests.get('https://www.windfinder.com/weatherforecast/salzgittersee')
+url = requests.get('https://www.windfinder.com/weatherforecast/salzgittersee')
 
-if r.status_code != 200:
+if url.status_code != 200:
     print("error while getting weather data")
 
 windArray = [] #stores all wind data in pairs of two, starting from min
@@ -18,24 +18,23 @@ minTime = 6 #time from which one can sail
 maxTime = 21 #time to which one can sail
 
 def sendEmail():
-    emailSender = keys.emailSenderOwn
-    emailReceiver = keys.emailReceiverOwn
-    emailPassword = keys.emailPasswordOwn
+    emailSender = keys.emailSenderOwn #put your own credentials here
+    emailReceiver = keys.emailReceiverOwn #put your own credentials here
+    emailPassword = keys.emailPasswordOwn #put your own credentials here
 
     smtp_server = smtplib.SMTP(keys.emailServerOwn, 587)
     smtp_server.ehlo()
     smtp_server.starttls()
     smtp_server.ehlo()
     smtp_server.login(emailSender, emailPassword)
-    print("logged in email")
     msg_to_send="""From: Siling reminder <{emailFrom}>\nTo: <{emailReceiver}>\nSubject: You should go siling today\nThe Wind is perfect at {weatherTime}\nHave fun!""".format(emailFrom = emailSender, emailReceiver = emailReceiver, weatherTime = sailingPossibilitys)
     print(msg_to_send)
     smtp_server.sendmail(emailSender, emailReceiver, msg_to_send)
     smtp_server.quit()
     print("email send")
 
-pulled = r.content.decode("utf-8")
-soup = BeautifulSoup(r.content, 'html.parser')
+pulled = url.content.decode("utf-8")
+soup = BeautifulSoup(url.content, 'html.parser')
 s = soup.select("div.weathertable__body > div:nth-of-type(8)")
 
 for index, wind in zip(range(48), soup.select("span.units-ws")) :
@@ -54,13 +53,9 @@ sailingPossibilitys = ""
 for x in range(0, 48, 2):
     if windArray[x] >= minWind and windArray[x+1] <= maxWind and rainArray[singleSteps] <= maxRain and countHour+1 >= minTime and countHour+1 <= maxTime:
         if countHour >= minHours:
-            print("go sail!", countHour+1)
-            #dataString = str(countHour+1) + " O clock with min: " + str(windArray[x]) + "(kts), max: " + str(windArray[x+1]) + "(kts) and " + str(rainArray[x]) + "(mm/h) rain"
             dataString = """\n{hour} o'clock with {mWind} - {xWind}(kts) of wind and {rain} (mm/h) of rain""".format(hour = countHour+1, mWind = windArray[x], xWind = windArray[x+1], rain = rainArray[singleSteps])
             sailingPossibilitys = sailingPossibilitys + dataString
             sail = True
-    else:
-        print("dont go sail", countHour+1)
 
     if singleSteps >= 0:
         countHour = singleSteps
